b8a6faa0937974e42d7e1e03fea05e05fad04dd6
EdgarCordero <edgar.cordero2890@alumnos.udg.mx>
2023-05-21 23:00:32 -0600

    Ultimas actualizaciones

---
+++ script.js
@@ -0,0 +1,11 @@
+function iniciarMap(){
+    var coord = {lat:20.654913 ,lng: -103.325788};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 15,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
